545dec0549c5a8a48f392490badf29e0425c3899
ggrachdev <ggrachdev@yandex.ru>
2021-05-20 12:05:45 +0400

    Add unit test skeleton

---
+++ ggrachdev.multiregionality/tests/UrlNormalizerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use UrlNormalizer
+
+class UrlNormalizerTest extends TestCase {
+
+    public function testNotice() {
+        
+        $this->assertTrue(false);
+    }
+
+}
