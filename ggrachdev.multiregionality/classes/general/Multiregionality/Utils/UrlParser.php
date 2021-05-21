ddbefea963ea0f309c86de06c543f7859f6b11d3
ggrachdev <ggrachdev@yandex.ru>
2021-05-21 14:49:02 +0400

    Add skeleton UrlParser and improve puml

---
+++ ggrachdev.multiregionality/classes/general/Multiregionality/Utils/UrlParser.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace GGrach\Multiregionality\Utils;
+
+class UrlParser {
+    public static function parse(string $url): array
+    {
+        return [];
+    }
+}
--- ggrachdev.multiregionality/include.php
+++ ggrachdev.multiregionality/include.php
@@ -1,5 +1,6 @@
 <?
 \Bitrix\Main\Loader::registerAutoLoadClasses('ggrachdev.multiregionality', [
     "\GGrach\Multiregionality\Utils\UrlNormalizer" => "classes/general/Multiregionality/Utils/UrlNormalizer.php",
+    "\GGrach\Multiregionality\Utils\UrlParser" => "classes/general/Multiregionality/Utils/UrlParser.php",
 ]);
 ?>
