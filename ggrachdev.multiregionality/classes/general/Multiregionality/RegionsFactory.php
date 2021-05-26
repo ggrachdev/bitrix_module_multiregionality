8332ea5da63961d0d3ee06532cdd072bd2f71a9a
ggrachdev <ggrachdev@yandex.ru>
2021-05-26 12:41:13 +0400

    Implemented Factory/Regions.php

--- ggrachdev.multiregionality/classes/general/Multiregionality/RegionsFactory.php
+++ ggrachdev.multiregionality/classes/general/Multiregionality/RegionsFactory.php
@@ -4,7 +4,9 @@ namespace GGrach\Multiregionality;
 
 use GGrach\Multiregionality\Facade\Regions;
 use GGrach\Multiregionality\Contract\IConfigurator;
-use GGrach\Multiregionality\Configurator;
+use GGrach\Multiregionality\Determinator\RegionDeterminator;
+use GGrach\Multiregionality\Repository\RegionsRepository;
+use GGrach\Multiregionality\Configurator\RegionsConfigurator;
 
 class RegionsFactory {
 
@@ -19,19 +21,23 @@ class RegionsFactory {
 
     public static function getInstance(string $url = null, IConfigurator $configurator = null): Regions {
         if ($url === null) {
-            $url = $_SERVER['DOCUMENT_URI'];
+            $url = $_SERVER['SERVER_NAME'];
         }
 
         if ($configurator === null) {
-            $url = new Configurator();
+            $configurator = new RegionsConfigurator();
         }
 
         $key = $url . $configurator;
         if (!isset(self::$instances[$key])) {
-            self::$instances[$key] = new Regions();
+
+            $repository = new RegionsRepository($configurator);
+            $determinator = new RegionDeterminator();
+
+            self::$instances[$key] = new Regions($url, $configurator, $repository, $determinator);
         }
 
-        return self::$instances[$cls];
+        return self::$instances[$key];
     }
 
 }
